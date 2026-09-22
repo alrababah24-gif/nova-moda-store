4ae2805d84e0fca18cbb32282ade2e970f9f8793
ABDALLAH ALRABABAH <alrababah24@gmail.com>
2026-09-22 13:53:23 +0300

    Fix Meta Pixel initialization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Tajawal } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 
 import { CartProvider } from "@/components/cart-provider";
@@ -77,12 +76,47 @@ const themeScript = `
     document.documentElement.style.colorScheme = isDark
       ? "dark"
       : "light";
-
   } catch (error) {
     document.documentElement.classList.remove("dark");
     document.documentElement.style.colorScheme = "light";
   }
 })();
+`;
+
+const metaPixelScript = `
+!function(f,b,e,v,n,t,s)
+{
+  if(f.fbq)return;
+
+  n=f.fbq=function(){
+    n.callMethod
+      ? n.callMethod.apply(n,arguments)
+      : n.queue.push(arguments)
+  };
+
+  if(!f._fbq)f._fbq=n;
+
+  n.push=n;
+  n.loaded=!0;
+  n.version='2.0';
+  n.queue=[];
+
+  t=b.createElement(e);
+  t.async=!0;
+  t.src=v;
+
+  s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s);
+}
+(
+  window,
+  document,
+  'script',
+  'https://connect.facebook.net/en_US/fbevents.js'
+);
+
+fbq('init', '1676852253417182');
+fbq('track', 'PageView');
 `;
 
 export default function RootLayout({
@@ -105,30 +139,9 @@ export default function RootLayout({
         />
 
         {/* Meta Pixel */}
-        <Script
-          id="meta-pixel"
-          strategy="afterInteractive"
+        <script
           dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;
-              n.push=n;
-              n.loaded=!0;
-              n.version='2.0';
-              n.queue=[];
-              t=b.createElement(e);
-              t.async=!0;
-              t.src=v;
-              s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}
-              (window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-
-              fbq('init', '1676852253417182');
-              fbq('track', 'PageView');
-            `,
+            __html: metaPixelScript,
           }}
         />
       </head>
@@ -147,7 +160,7 @@ export default function RootLayout({
           />
         </noscript>
 
-        {/* Track navigation between Next.js pages */}
+        {/* تتبع التنقل داخل الموقع */}
         <MetaPixel />
 
         <CartProvider>
