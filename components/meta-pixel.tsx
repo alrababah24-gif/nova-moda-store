bd37600e51662e4acec54645d8e8ef320ff05b8e
alrababah24-gif <alrababah24@gmail.com>
2026-09-23 16:27:10 +0300

    Update meta-pixel.tsx

--- components/meta-pixel.tsx
+++ components/meta-pixel.tsx
@@ -1,29 +1,39 @@
 "use client";
+import Script from "next/script";
 
-import { useEffect, useRef } from "react";
-import { usePathname } from "next/navigation";
-
-declare global {
-  interface Window {
-    fbq?: (...args: any[]) => void;
-  }
-}
+const PIXEL_ID = "1676852253417182";
 
 export default function MetaPixel() {
-  const pathname = usePathname();
-  const firstLoad = useRef(true);
-
-  useEffect(() => {
-    // أول PageView محسوب من الكود الرئيسي
-    if (firstLoad.current) {
-      firstLoad.current = false;
-      return;
-    }
-
-    if (typeof window.fbq === "function") {
-      window.fbq("track", "PageView");
-    }
-  }, [pathname]);
-
-  return null;
+  return (
+    <>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${PIXEL_ID}');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+    </>
+  );
 }
+
